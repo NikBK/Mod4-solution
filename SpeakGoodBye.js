ac3716fa88aa8a6c2886f8cb5cc4e320d5de657f
NikBK <nikhilbalakrishna5@gmail.com>
2020-07-11 17:48:14 +0530

    add these 1st edits

---
+++ SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function () {
+	var byeSpeaker = {speak()};
+	var speakWord = "Good Bye";
+	function speak(name) {
+  		console.log(speakWord + " " + name);
+	}
+
+})();
+
---
+++ script.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (/* fill in parts of the 'for' loop to loop over names array */) {
+	var firstLetter =
+if (/* fill in condition here */) {
+    // byeSpeaker.xxxx
+  } else {
+    // helloSpeaker.xxxx
+  }
+}
